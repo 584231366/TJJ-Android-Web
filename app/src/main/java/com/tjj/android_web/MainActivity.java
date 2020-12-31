22e3a429d1a64d63d1631dc9f84c91d9c2f566a1
唐茄茄 <584231366@qq.com>
2020-12-31 20:41:15 +0800

    Signed-off-by: 唐茄茄 <584231366@qq.com>
    - 初始化android项目

---
+++ app/src/main/java/com/tjj/android_web/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tjj.android_web;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
